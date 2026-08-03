cfbd6e510f17f227a1ee4a7d94b2e5736ce39057
johnclamy <johnclamy@gmail.com>
2026-08-03 06:28:06 +0100

    set the web component to receive the required service

--- backend/src/web/users.py
+++ backend/src/web/users.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 from model.users import User
-import data.users as service
+import services.users as service
 import uuid
 
 
@@ -11,21 +11,26 @@ router = APIRouter(prefix="/users")
 def get_all() -> list[User]:
     return service.get_all()
 
+
 @router.get("/{user_id}")
 def get_by_id(user_id: str) -> User | None:
     return service.get_by_id(uuid.UUID(user_id))
+
 
 @router.post("/")
 def create(user: User) -> User:
     return service.create(user)
 
+
 @router.patch("/{user_id}")
 def modify(user_id: str, user: User) -> User | None:
     return service.modify(uuid.UUID(user_id), user)
 
+
 @router.put("/{user_id}")
 def replace(user_id: str, user: User) -> User | None:
     return service.replace(uuid.UUID(user_id), user)
+
 
 @router.delete("/{user_id}")
 def delete(user_id: str) -> bool:
